c9a9f0d6c07950ef95dbdeefcc68909c7e580c7a
Фаридун <lkromov.faridun@itgroup.kz>
2022-08-23 21:35:35 +0500

    documented by swagger

--- app/Http/Controllers/Api/V1/NotebookController.php
+++ app/Http/Controllers/Api/V1/NotebookController.php
@@ -12,22 +12,50 @@ use Illuminate\Support\Facades\File;
 
 class NotebookController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/v1/NotebookController",
+     *     description="returns all notebooks with pagination 10 ordered by latest",
+     *     @OA\Response(response="200", description="collection of Notebooks")
+     * )
+     */
     public function index()
     {
         return NotebookResource::collection(Notebook::latest()->paginate(10));
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/v1/NotebookController",
+     *     description="adds to database a new Notebook",
+     *     @OA\Response(response="200", description="returns added Notebook")
+     * )
+     */
     public function store(NoteBookStoreRequest $request, NotebookStoreAction $action)
     {
 
         return new NotebookResource($action->handle($request));
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/v1/NotebookController/{id}",
+     *     description="gets the Notebook by id",
+     *     @OA\Response(response="200", description="returns a Notebook by id")
+     * )
+     */
     public function edit($id)
     {
         return new NotebookResource(Notebook::firstWhere('id', $id));
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/v1/NotebookController/{id}",
+     *     description="updates a Notebook by id",
+     *     @OA\Response(response="200", description="returns updated Notebook by id")
+     * )
+     */
     public function update(NoteBookStoreRequest $request, $id, NotebookUpdateAction $action)
     {
         $action->handle($request, $id);
@@ -35,6 +63,13 @@ class NotebookController extends Controller
         return new NotebookResource(Notebook::firstWhere('id', $id));
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/v1/NotebookController/{id}",
+     *     description="deletes a Notebook by id",
+     *     @OA\Response(response="200", description="returns no content")
+     * )
+     */
     public function destroy($id)
     {
         $notebook = Notebook::firstWhere('id', $id);
